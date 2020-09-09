3617801436263f5a897838872871fbd913846974
unholyFigaro <71033284+unholyFigaro@users.noreply.github.com>
2020-09-09 20:20:22 +0300

    Add files via upload

---
+++ ConsoleApplication74.cpp
@@ -0,0 +1,77 @@
+﻿#include <iostream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+
+
+class Detail {
+private:
+    int id=0, tmp=0;
+    string name;
+public:
+    friend class Controller;
+    void defaultIdSetter(){
+        int defaultId = 1111111;
+        id = defaultId;
+    }
+    int idSetter(int number){
+        if (number > 9999999 || number < 1000000) {
+            cout << "Input number must be >1000000 and <9999999" << endl;
+            return 1;
+        }
+        tmp = number;
+        return 0;
+    }
+    void nameSetter(string userName) {
+        name = userName;
+        return;
+    }
+    void printDetailInfo() {
+        cout << "Id:" << id << endl;
+        cout << name;
+    }
+};
+
+class Controller {
+public:
+    int ControllerIdSetter(Detail &object){
+            object.id = object.tmp;
+            return 0;
+    }
+};
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    
+    Detail object;
+    
+    object.defaultIdSetter();
+    string userName;
+    
+    cin >> userName;
+    
+    object.nameSetter(userName);
+    object.printDetailInfo();
+    
+    Detail objects[3];
+    Controller objControl;
+    for (int i = 0; i < 3; i++) {
+        cout <<endl <<"Enter the name of " << i + 1 << "detail" << endl;
+        cin >> userName;
+        objects[i].nameSetter(userName);
+        cout << "Enter id of " << i + 1 << "detail (100000 < x < 9999999)" << endl;
+        int number;
+        cin >> number;
+        while (objects[i].idSetter(number) != 0) {
+            cout << "Reenter ID: " << endl;
+            cin >> number;
+            objects[i].idSetter(number);
+        }
+        objControl.ControllerIdSetter(objects[i]);
+        objects[i].printDetailInfo();
+    }
+}
